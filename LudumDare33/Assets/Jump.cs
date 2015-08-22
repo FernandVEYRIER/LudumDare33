abb247f1bcfe8da67a2bd9d389feedf25db06a3d
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2015-08-22 13:04:38 +0200

    BasicController

---
+++ LudumDare33/Assets/Jump.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	private GameObject character;
+	// Use this for initialization
+	void Start () {
+
+		character = this.transform.parent.gameObject;
+	}
+	
+	void OnTriggerStay2D(Collider2D col) {
+
+		if (col.CompareTag("ground")) {
+			character.GetComponent<BasicController> ().setCanJump (true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+
+		if (col.tag == "ground") {
+			character.GetComponent<BasicController> ().setCanJump (false);
+		}
+	}
+}
