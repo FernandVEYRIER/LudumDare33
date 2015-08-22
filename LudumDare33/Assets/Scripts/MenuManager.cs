4ff0e4f1dfbcd4893f9118887eecedf84792fb6e
Fernand <fernand.veyrier@epitech.eu>
2015-08-22 12:00:37 +0200

    menu manette
    Signed-off-by: Fernand <fernand.veyrier@epitech.eu>

--- LudumDare33/Assets/Scripts/MenuManager.cs
+++ LudumDare33/Assets/Scripts/MenuManager.cs
@@ -1,8 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour {
-	
+
+	public EventSystem es;
+	public GameObject [] buttons;
+
+	private int currentButtonSelected;
+
+	void Start()
+	{
+		currentButtonSelected = 0;
+		ChangeSelectedButton( currentButtonSelected );
+	}
+
+	public void ChangeSelectedButton( int id )
+	{
+		es.SetSelectedGameObject( buttons[id] );
+	}
+
 	public void LoadLevel( int level )
 	{
 		Application.LoadLevel( level );
